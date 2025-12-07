56e49144e649361084f2c329a6d37d071bbb49a4
kbaicai <kbaicai@qq.com>
2025-12-07 10:00:50 -0500

    添加构建测试脚本

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -21,19 +21,15 @@ subprojects {
 
 // 为缺少 namespace 的旧插件添加补丁（isar_flutter_libs 等）
 subprojects {
-    afterEvaluate {
-        if (project.hasProperty("android")) {
-            val android = project.extensions.findByName("android")
-            if (android is com.android.build.gradle.LibraryExtension) {
-                if (android.namespace == null) {
-                    // 为已知的旧插件指定 namespace
-                    val namespaceMap = mapOf(
-                        "isar_flutter_libs" to "dev.isar.isar_flutter_libs"
-                    )
-                    android.namespace = namespaceMap[project.name]
-                        ?: "com.example.${project.name.replace("-", "_")}"
-                }
-            }
+    plugins.withId("com.android.library") {
+        val android = extensions.getByType(com.android.build.gradle.LibraryExtension::class.java)
+        if (android.namespace == null) {
+            // 为已知的旧插件指定 namespace
+            val namespaceMap = mapOf(
+                "isar_flutter_libs" to "dev.isar.isar_flutter_libs"
+            )
+            android.namespace = namespaceMap[project.name]
+                ?: "com.example.${project.name.replace("-", "_")}"
         }
     }
 }
